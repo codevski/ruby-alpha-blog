76fff5613e0d7f7947dfeb37375e0538ed9a4412
Codevski <1435321+codevski@users.noreply.github.com>
2019-04-11 14:24:22 +1000

    Style blog index page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,7 +35,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
