ff04e7840d2b26bd1d10f327e1de0487321634a7
Codevski <1435321+codevski@users.noreply.github.com>
2019-04-16 22:10:09 +1000

    Fixed syntax for require_same_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,7 @@ before_action :require_same_user, only: [:edit, :update]
     end
 
     def require_same_user
-      if current_user =! @user
+      if current_user != @user
         flash[:danger] = "You can only edit your own account"
         redirect_to root_path
       end
